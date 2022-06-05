4105bed9c49221c5c9b9ce2cad8ee3f0bf8ab914
Silvio Paiva <silvioej@gmail.com>
2022-06-05 19:27:33 -0300

    progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Grid, GridItem } from './components/Grid';
+import styled from 'styled-components';
+import { ProgressBar } from './components/Progress';
 
+
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: ${props => props.theme.light.bg};
+`
+
+
+const Button = styled.button`
+  width: 100px;
+  height: 30px;
+  background: ${props => props.theme.dark.bg};
+  color: ${props => props.theme.light.bg};
+`
 
 function App() {
+
+  const themes = {
+    light: {
+      bg: 'white'
+    },
+    dark: {
+      bg: 'black'
+    }
+
+  }
+
+  const [theme, setTheme] = useState<any>(themes);
+
+  function changeTheme() {
+    switch (sessionStorage.getItem('actual-theme')) {
+      case 'light':
+        setTheme(themes)
+        sessionStorage.setItem('actual-theme', 'dark')
+        break;
+      case 'dark':
+        const darkTheme = {light: themes.dark, dark: themes.light}
+        setTheme(darkTheme)
+        sessionStorage.setItem('actual-theme', 'light')
+        break;
+      default:
+        setTheme(themes);
+        sessionStorage.setItem('actual-theme', 'light')
+        break;
+    }
+  }
+
+  // useEffect(() => {
+  //   changeTheme();
+  // }, [])
+
+
+  const [progress,setProgress] = useState(0);
+
+  useEffect(() => {
+    setTimeout(()=>{
+      if(progress < 100){
+        const num = (progress + 10) < 100 ? progress + 15 : 100
+        setProgress(num)
+      }
+    }, 1000)
+  },[progress])
+
   return (
-    <div id="container">
-      <Grid>
-        <GridItem cols={3} rows={2}></GridItem>
-      </Grid>
-    </div>
+
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Grid>
+          <GridItem justify='center' align='center' style={{padding: '0 5px'}}>
+            <ProgressBar height='25px' width='12vw' progress={progress} clr='#F0F' mode={'middle'} showProgress/>
+          </GridItem>
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Progress/index.tsx
@@ -0,0 +1,75 @@
+import styled, { css } from "styled-components";
+import { ProgressProps } from "./types";
+
+
+const getRGBcolor = ({ clr, progress, mode }: ProgressProps) => {
+
+    if (!mode) return clr
+
+    let r = NaN;
+    let g = NaN;
+    let b = NaN;
+
+    if (clr?.length === 7) {
+        r = parseInt(clr?.slice(1, 3), 16);
+        g = parseInt(clr?.slice(3, 5), 16);
+        b = parseInt(clr?.slice(5, 7), 16);
+    }
+    if (clr?.length === 4) {
+        r = parseInt(clr?.slice(1, 2) + clr?.slice(1, 2), 16);
+        g = parseInt(clr?.slice(2, 3) + clr?.slice(2, 3), 16);
+        b = parseInt(clr?.slice(3, 4) + clr?.slice(2, 3), 16);
+    }
+    if (isNaN(r) || isNaN(g) || isNaN(b)) {
+        return 'red'
+    }
+    return `linear-gradient(${mode === 'left' ? 'to left' : 'to right'},rgb(${r} ${g} ${b} / 0.3) 0% ,${mode === 'middle' ? `rgb(${r} ${g} ${b} / ${progress / 100 + 0.3}) 50%,rgb(${r} ${g} ${b} / 0.3) 100%)` : `rgb(${r} ${g} ${b} / ${progress / 100 + 0.3}) 100%)`}`
+}
+
+
+const borderHelper = css`
+    border-radius: 15px;
+`
+
+const before = ({progress,showProgress}: ProgressProps) => css`
+    &:before{
+        content: '';
+        text-align: center;
+        align-content: center;
+        position:absolute;
+        width: ${progress.toFixed(1)}%;
+        height: 100%;
+        background: ${getRGBcolor};
+        transition: width 1s;
+        ${borderHelper}
+    }
+    ${showProgress ?
+    `&:after{
+        content: '${progress < 100 ? progress.toFixed(2) : progress?.toFixed(0)}%';
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: transparent;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: 700;
+        font-size: 10px;
+    }` : ''
+}
+    
+`
+
+export const ProgressBar = styled.div<ProgressProps>`
+    position:relative;
+    width: ${({ width }) => width ? width : '100%'};
+    height:${({ height }) => height ? height : '100%'};
+    overflow: hidden;
+    background: white;
+    ${before}
+    ${borderHelper}
+    
+    
+`
+
+
